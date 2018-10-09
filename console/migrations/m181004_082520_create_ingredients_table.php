1258dffa84af6fb16609e2f7aa7f80206bee4525
Александр <shlykovn@mail.ru>
2018-10-09 15:05:37 +0300

    migrations

--- console/migrations/m181004_082520_create_ingredients_table.php
+++ console/migrations/m181004_082520_create_ingredients_table.php
@@ -16,8 +16,8 @@ class m181004_082520_create_ingredients_table extends Migration
             'id' => $this->primaryKey()->unsigned(),
             'name' => $this->string(255),
             'description' => $this->text(),
-	        'dt_add' => $this->dateTime(),
             'status' => $this->tinyInteger(3)->unsigned(),
+            'dt_add' => $this->dateTime(),
         ]);
     }
 
